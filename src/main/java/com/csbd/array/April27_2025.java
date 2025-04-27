d3de7e2f4c7c79c5cabb537d987cd35b4e4c7e25
Nazmul-81 <nazmul81csekuet@gmail.com>
2025-04-27 08:27:01 +0600

    solved an easy problem

---
+++ src/main/java/com/csbd/array/April27_2025.java
@@ -0,0 +1,13 @@
+package com.csbd.array;
+
+public class April27_2025 {
+    public int countSubarrays(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if ((nums[i] + nums[i + 2]) * 2 == nums[i + 1]) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
