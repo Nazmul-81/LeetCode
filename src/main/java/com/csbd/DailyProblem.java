46b55b95c901b0786a92e574388cd68983974d55
Nazmul-81 <nazmul81csekuet@gmail.com>
2025-05-24 08:44:42 +0600

    solved an easy problem on string

--- src/main/java/com/csbd/DailyProblem.java
+++ src/main/java/com/csbd/DailyProblem.java
@@ -110,6 +110,20 @@ public class DailyProblem {
         return true;
     }
 
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            for (char c : word.toCharArray()) {
+                if (c == x) {
+                    ans.add(i);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
 
 
     public static void main(String[] args) {
